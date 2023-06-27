5e902c73993b456753ae22cc2b53675223a9e4cc
Edward Tanguay (windows) <edward@tanguay.info>
2023-06-28 01:36:57 +0200

    finished

--- src/components/Searchbox.jsx
+++ src/components/Searchbox.jsx
@@ -1,12 +1,20 @@
+import { useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { ImSearch } from "react-icons/im";
 import { AppContext } from "./Context";
 
 const Searchbox = () => {
-  const {filter,setFilter}=useContext(AppContext)
+  const { filter, setFilter } = useContext(AppContext);
+  const navigate = useNavigate();
+
+  const handleSearchBoxChange = (text) => {
+    navigate('/product');
+    setFilter(text);
+  };
+
   return (
     <span className="bg-gray-700 flex items-center py-1 pl-2 pr-4 rounded-3xl max-md:hidden">
-      <input className="rounded-3xl " type="text" placeholder="search here" value={filter} onChange={(e)=>{setFilter(e.target.value)}} />
+      <input className="rounded-3xl " type="text" placeholder="search here" value={filter} onChange={(e) => { handleSearchBoxChange(e.target.value); }} />
       <ImSearch className="ml-2 text-white" />
     </span>
   );
